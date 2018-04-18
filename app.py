77705b7bbee8d197bfeee2730f8c002cb4eb62ba
root <root@vps.purpl3.net>
2018-04-18 15:55:12 +0200

    Remove old code and update /api

--- app.py
+++ app.py
@@ -8,13 +8,6 @@ import base64
 import re
 import uuid
 
-
-def generatePassword(alias, secret, length):
-    plainPassword = alias + secret
-    sha1Password = hashlib.sha512(plainPassword.strip().encode("UTF-8"))
-    b64Password = base64.b64encode(bytearray(sha1Password.hexdigest(),"UTF-8"))
-    return b64Password[:length].decode("UTF-8")
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -40,12 +33,18 @@ def my_form_post():
     else:
         return render_template("index.html", password = "Not able to calculate with empty values.")
 @app.route('/api')
-def api(alias = "", secret = "", size = 16):
-    alias = request.args.get('alias', alias)
+def api(alias = "", secret = ""):
+	alias = request.args.get('alias', alias)
     secret = request.args.get('secret', secret)
-    size = request.args.get('size', size)
-    pw = generatePassword(alias, secret, int(size))
-    return pw
+    client = docker.from_env()
+    container_uuid = "runner"+uuid.uuid4().hex
+    response = client.containers.run("ckevi/pwcalc-runner",
+        name=container_uuid,
+        labels=[container_uuid],
+        environment={"PWCALC_ALIAS": alias, "PWCALC_SECRET": secret})
+    response_text = response.decode("UTF-8")
+    client.containers.prune(filters={"label": container_uuid})
+    return response_text
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=3333)
