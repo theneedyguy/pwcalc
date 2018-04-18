e4600ee4c21e349635a802a3464e8d514dc7fa46
root <root@vps.purpl3.net>
2018-04-18 15:59:27 +0200

    Updated docker image

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build . -t ckevi/pwcalc:latest
-docker tag ckevi/pwcalc:latest ckevi/pwcalc:2.0-root
+docker tag ckevi/pwcalc:latest ckevi/pwcalc:2.1-root
 docker push ckevi/pwcalc:latest
-docker push ckevi/pwcalc:2.0-root
+docker push ckevi/pwcalc:2.1-root
