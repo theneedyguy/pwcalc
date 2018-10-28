53193dd9cbc1cfd3a8d3f45dc7985145a63b87c3
theneedyguy <admin@purpl3.net>
2018-10-28 11:33:30 +0100

    updated version number

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build . -t ckevi/pwcalc:latest
-docker tag ckevi/pwcalc:latest ckevi/pwcalc:3.0-root
+docker tag ckevi/pwcalc:latest ckevi/pwcalc:3.0.1-root
 docker push ckevi/pwcalc:latest
-docker push ckevi/pwcalc:3.0-root
+docker push ckevi/pwcalc:3.0.1-root
